298f71c018a0288f3f4367aaa600792971623895
foobar <uagnl25x@anonaddy.me>
2023-03-06 21:37:48 +0530

    default nvchad files 	modified:   .gitignore 	new file:   .stylua.toml 	new file:   LICENSE 	modified:   init.lua 	deleted:    lazy-lock.json 	new file:   lua/core/default_config.lua 	new file:   lua/core/init.lua 	new file:   lua/core/lazy_load.lua 	new file:   lua/core/mappings.lua 	new file:   lua/core/options.lua 	new file:   lua/core/utils.lua 	deleted:    lua/plugins/autopairs.lua 	deleted:    lua/plugins/autosession.lua 	deleted:    lua/plugins/cmp.lua 	new file:   lua/plugins/configs/alpha.lua 	new file:   lua/plugins/configs/cmp.lua 	new file:   lua/plugins/configs/lspconfig.lua 	new file:   lua/plugins/configs/mason.lua 	new file:   lua/plugins/configs/nvimtree.lua 	new file:   lua/plugins/configs/nvterm.lua 	new file:   lua/plugins/configs/others.lua 	new file:   lua/plugins/configs/telescope.lua 	new file:   lua/plugins/configs/treesitter.lua 	new file:   lua/plugins/configs/whichkey.lua 	deleted:    lua/plugins/dashboard.lua 	new file:   lua/plugins/init.lua 	deleted:    lua/plugins/lsp.lua 	deleted:    lua/plugins/lualine.lua 	deleted:    lua/plugins/noice.lua 	deleted:    lua/plugins/notify.lua 	deleted:    lua/plugins/nvim-tree.lua 	deleted:    lua/plugins/tabline.lua 	deleted:    lua/plugins/telescope.lua 	deleted:    lua/plugins/treesitter.lua 	deleted:    lua/usr/autocommands.lua 	deleted:    lua/usr/keymaps.lua 	deleted:    lua/usr/options.lua 	deleted:    lua/usr/prettyfold.lua

--- init.lua
+++ init.lua
@@ -1,121 +1,34 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-                "git",
-                "clone",
-                "--filter=blob:none",
-                "https://github.com/folke/lazy.nvim.git",
-                "--branch=stable", -- latest stable release
-                lazypath,
-        })
+vim.defer_fn(function()
+  pcall(require, "impatient")
+end, 0)
+
+require "core"
+require "core.options"
+
+-- setup packer + plugins
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+  print "Cloning packer .."
+  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+
+  -- install plugins + compile their configs
+  vim.cmd "packadd packer.nvim"
+  require "plugins"
+  vim.cmd "PackerSync"
+
+  -- install binaries from mason.nvim & tsparsers
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "PackerComplete",
+    callback = function()
+      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
+      require("packer").loader "nvim-treesitter"
+    end,
+  })
 end
-vim.opt.rtp:prepend(lazypath)
 
-require 'lazy'.setup({
-        {'folke/zen-mode.nvim'},
-        {
-                -- auto complete stuff
-                'ms-jpq/coq_nvim',
-                enabled = false,
-                dependencies = { 'ms-jpq/coq.artifacts' },
-                init = function()
-                        vim.g.coq_settings = { auto_start = true }
-                end,
-        },
-        {
-                "anuvyklack/pretty-fold.nvim",
-                enabled = false,
-                dependencies = { 'anuvyklack/fold-preview.nvim', 'anuvyklack/keymap-amend.nvim' }
-        },
-        {
-                "iamcco/markdown-preview.nvim", -- Markdown preview
-                -- build = '',
-                -- cond = function()
-                --   return vim.fn.executable 'make' == 1
-                -- end,
-        },
-        {
-                "jackMort/ChatGPT.nvim",
-                dependencies = {
-                        "MunifTanjim/nui.nvim",
-                        "nvim-lua/plenary.nvim",
-                        "nvim-telescope/telescope.nvim"
-                },
-                opts = {}
-        },
-        {
-                -- Add indentation guides even on blank lines
-                'lukas-reineke/indent-blankline.nvim',
-                -- See `:help indent_blankline.txt`
-                opts = {
-                        char = '┊',
-                        show_trailing_blankline_indent = false,
-                },
-        },
-        {
-                -- Adds git releated signs to the gutter, as well as utilities for managing changes
-                'lewis6991/gitsigns.nvim',
-                opts = {
-                        -- See `:help gitsigns.txt`
-                        signs = {
-                                add = { text = '+' },
-                                change = { text = '~' },
-                                delete = { text = '_' },
-                                topdelete = { text = '‾' },
-                                changedelete = { text = '~' },
-                        },
-                },
-        },
-        {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                build = 'make',
-                cond = function()
-                        return vim.fn.executable 'make' == 1
-                end,
-        },
-        {
-                'chentoast/marks.nvim',
-                opts = {}
-        },
-        { "cbochs/portal.nvim" }, -- https://github.com/cbochs/portal.nvim#portals
-        { 'folke/which-key.nvim',    opts = {} },
-        { "mfussenegger/nvim-dap" },
-        { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
-        { 'akinsho/toggleterm.nvim', version = "*", opts = {} },
-        {
-                "ggandor/leap.nvim",
-                dependencies = { "tpope/vim-repeat" },
-                config = function()
-                        local leap = require('leap')
-                        leap.add_default_mappings()
-                        leap.opts.case_sensitive = false
-                end
-        },
+pcall(require, "custom")
 
-        {
-                'numToStr/Comment.nvim',
-                dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-                event = "VeryLazy",
-                config = function()
-                        require("Comment").setup({
-                                pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-                        })
-                end
-        },
-        {
-                -- colorschemes
-                -- 'sainnhe/sonokai',
-                -- 'navarasu/onedark.nvim',
-                'folke/tokyonight.nvim',
-                priority = 1000,
-                config = function()
-                        vim.cmd.colorscheme 'tokyonight-night'
-                end,
-        },
-        { import = 'plugins' },
-})
-require("usr.keymaps")
-require("usr.options")
-require('usr.autocommands')
+require("core.utils").load_mappings()
